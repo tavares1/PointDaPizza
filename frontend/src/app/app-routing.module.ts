54d8227e53a01c56ffdfbef06608a77f2211f8ff
tavares1 <lucastavaresvasconcelos@gmail.com>
2018-02-18 21:04:03 -0300

    corrigindo erros indicados pelo tslint

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule,Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { CardapioComponent } from './modules/cardapio/cardapio/cardapio.component';
 import { PedidoComponent } from './modules/pedido/pedido/pedido.component';
 import { HomeComponent } from './modules/home/home/home.component';
