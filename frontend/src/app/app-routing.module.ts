2bea9e9d98e3512e81e5524118dbcae48cfb1235
tavares1 <lucastavaresvasconcelos@gmail.com>
2018-02-18 21:29:37 -0300

    corrigindo arquitetura do footer

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,11 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CardapioComponent } from './modules/cardapio/cardapio/cardapio.component';
 import { PedidoComponent } from './modules/pedido/pedido/pedido.component';
 import { HomeComponent } from './modules/home/home/home.component';
-<<<<<<< HEAD
-import { FooterComponent } from './modules/footer/footer/footer.component';
-=======
 import {LoginComponent} from './modules/login/login/login.component';
->>>>>>> 54d8227e53a01c56ffdfbef06608a77f2211f8ff
 
 const routes: Routes = [
   {path: '', component: HomeComponent, pathMatch: 'full'},
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,24 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {HttpClientModule, HttpClient} from '@angular/common/http';
 
-import { AppRoutingModule } from './/app-routing.module';
-import { CardapioModule } from './modules/cardapio/cardapio.module';
-import { PedidoModule} from './modules/pedido/pedido.module';
-import {HomeModule} from './modules/home/home.module';
-<<<<<<< HEAD
-import {FooterModule} from './modules/footer/footer.module';
-=======
-import {LoginModule} from './modules/login/login.module';
->>>>>>> 54d8227e53a01c56ffdfbef06608a77f2211f8ff
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
 import { MenuComponent } from './components/menu/menu.component';
-import { FooterComponent } from './components/footer/footer.component';
+
+import {AppRoutingModule} from './/app-routing.module';
+import {CardapioModule} from './modules/cardapio/cardapio.module';
+import {PedidoModule} from './modules/pedido/pedido.module';
+import {HomeModule} from './modules/home/home.module';
+import {LoginModule} from './modules/login/login.module';
+import {FooterComponent} from './components/footer/footer.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +27,6 @@ import { FooterComponent } from './components/footer/footer.component';
     CardapioModule,
     PedidoModule,
     HomeModule,
-    FooterModule,
     NgbModule.forRoot(),
     LoginModule,
   ],
